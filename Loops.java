835d19cc081d7d483ae34c364fdb26e6cb5dd84a
Yarram Gowtham Kumar Reddy <120354964+Ralpfh@users.noreply.github.com>
2024-06-24 11:49:24 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,25 @@
+public class LoopStudy {
+
+    public static void main(String[] args) {
+        System.out.println("for loop");
+      // For Loop
+        for(int i=0;i<100;i++){
+            System.out.println(i);
+        }
+      
+        System.out.println("while loop");
+        int i=1;
+      //While Loop
+        while(i<10){
+            System.out.println("Hello");
+            i++;
+        }
+        System.out.println("do while loop");
+        int n=4;
+      // do While Loop
+        do{
+            System.out.println(n);
+            n--;
+        }while(n>5);
+    }
+}
